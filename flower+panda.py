4f5c6c5db5e03279d86c5d470ff2ba8019d3ce4e
Li Tong <66765124+litonglow@users.noreply.github.com>
2021-04-15 10:26:10 +0800

    add flowers

---
+++ flower+panda.py
@@ -0,0 +1,149 @@
+# -*- coding: utf-8 -*-
+"""
+Bye Reine :(
+@author: m.siddharthan
+"""
+#To close turtle window type turtle.bye()
+import turtle
+from random import randint
+
+screen = turtle.Screen()
+screen.setup(700, 700)
+screen.bgcolor("#f7f7f7")
+turtle.speed(0)
+turtle.title('Farewell Reine!')
+
+#flower
+def draw_flower (turtle, color, x, y):
+    for i in range(5): #no of petals
+        turtle.penup()
+        turtle.color(color)
+        turtle.goto(x,y)
+        turtle.pendown()    
+        turtle.begin_fill()
+        heading = turtle.heading()
+        turtle.circle(10, 90)
+        turtle.left(90)
+        turtle.circle(10, 90)
+        turtle.setheading(heading)
+        turtle.left(72) #360/no of petals
+        turtle.end_fill()
+        turtle.hideturtle()  
+#flower rain
+x=0
+y=0
+j=0
+turtle.colormode(255)
+color=(randint(0, 255), randint(0, 255), randint(0, 255))
+while j <10:
+    draw_flower(turtle, color, x, y)	
+    x=randint(-350,350)
+    y=randint(-350,350)
+    print(x,y)
+    j+=1
+    color=(randint(0, 255), randint(0, 255), randint(0, 255))
+
+#Background
+turtle.penup()
+turtle.goto(0, -300)
+turtle.pendown()
+turtle.color("#d9337a")
+turtle.begin_fill()
+turtle.circle(450)
+turtle.end_fill()
+
+#Panda
+#nose
+turtle.speed(0)
+turtle.penup()
+turtle.goto(-60,0)
+turtle.pendown()
+turtle.color("white")
+def draw_triangle(length):
+    for i in range(3):
+        turtle.forward(length)
+        turtle.right(120)
+turtle.begin_fill()
+draw_triangle(120)
+turtle.end_fill()
+
+#right eye
+turtle.penup()
+turtle.goto(150,50)
+turtle.pendown()
+turtle.color("white")
+turtle.begin_fill()
+turtle.circle(90)
+turtle.end_fill()
+
+turtle.penup()
+turtle.goto(150,50)
+turtle.pendown()
+turtle.color("#c5c5c5")
+turtle.begin_fill()
+turtle.circle(50)
+turtle.end_fill()
+
+turtle.penup()
+turtle.goto(150,70)
+turtle.pendown()
+turtle.color("white")
+turtle.begin_fill()
+turtle.circle(10)
+turtle.end_fill()
+
+#left eye
+turtle.penup()
+turtle.goto(-150,50)
+turtle.pendown()
+turtle.color("white")
+turtle.begin_fill()
+turtle.circle(90)
+turtle.end_fill()
+
+turtle.penup()
+turtle.goto(-150,50)
+turtle.pendown()
+turtle.color("#c5c5c5")
+turtle.begin_fill()
+turtle.circle(50)
+turtle.end_fill()
+
+turtle.penup()
+turtle.goto(-150,70)
+turtle.pendown()
+turtle.color("white")
+turtle.begin_fill()
+turtle.circle(10)
+turtle.end_fill()
+
+#smile left
+turtle.penup()
+turtle.goto(0,-90)
+turtle.pendown()
+turtle.pensize(10)
+turtle.color("white")
+turtle.setheading(90)
+turtle.circle(100,-130)
+
+#smile right
+turtle.penup()
+turtle.goto(0,-90)
+turtle.pendown()
+turtle.pensize(10)
+turtle.color("white")
+turtle.setheading(-90)
+turtle.circle(100,130)
+
+#insert textbox
+turtle.penup()
+turtle.color("Black")
+turtle.goto(-300, -300)
+turtle.write("We will miss you give us food!!", font=("Exo", 25, "bold"))
+turtle.penup()
+turtle.color("Black")
+turtle.goto(-250, -330)
+turtle.write("~Love, CRADLE", font=("Exo", 20, "italic"))
+turtle.hideturtle()  
+
+turtle.Screen().exitonclick()
